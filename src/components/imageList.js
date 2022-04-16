0a6787c80372e4108b62509ae7970d46e94252b4
Levi Stephen <levisteveen@gmail.com>
2022-04-16 14:43:28 +0100

    App completed

--- src/components/imageList.js
+++ src/components/imageList.js
@@ -1,12 +1,15 @@
+import './imageList.css';
 import React from "react";
+import ImageCard from './ImageCard';
+
 
 const ImageList = (props) => {
-    const images = props.images.map(({id, description, urls}) => {
-            return <img key={id} src={urls.regular} alt={description} />
+    const images = props.images.map((image) => {
+            return < ImageCard key={image.id} image={image} />
     });
 
     return (
-        <div>{images}</div>
+        <div className='image-list' >{images}</div>
     );
 };
 
